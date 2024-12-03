40bf77b2680969b3dd73102d186bd67614710cd0
emr3k0c <emr3k0c@yahoo.com>
2024-12-03 23:22:37 +0300

    initialize general structure

---
+++ src/main/java/com/rokue/game/Main.java
@@ -0,0 +1,8 @@
+package com.rokue.game;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to RoKUe-Like Game!");
+        // Entry point for the game
+    }
+}
