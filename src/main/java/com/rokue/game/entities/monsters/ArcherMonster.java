6c7d9976732e80d6870b645449c5b1093f431104
Alperen Kars <sachr@Alperen-MacBook-Air.local>
2024-12-05 22:41:44 +0300

    monsters added

---
+++ src/main/java/com/rokue/game/entities/monsters/ArcherMonster.java
@@ -0,0 +1,23 @@
+package com.rokue.game.entities.monsters;
+
+import com.rokue.game.entities.Hall;
+import com.rokue.game.entities.Hero;
+import com.rokue.game.util.Position;
+
+class ArcherMonster extends Monster {
+    public ArcherMonster(Position startPosition) {
+        super(startPosition);
+    }
+
+    @Override
+    public void update(Hero hero, Hall hall) {
+        System.out.println("ArcherMonster: Shooting arrows!");
+        if (distanceTo(hero.getPosition()) < 4) {
+            hero.decreaseLife();
+        }
+    }
+
+    private int distanceTo(Position heroPosition) {
+        return Math.abs(position.getX() - heroPosition.getX()) + Math.abs(position.getY() - heroPosition.getY());
+    }
+}
