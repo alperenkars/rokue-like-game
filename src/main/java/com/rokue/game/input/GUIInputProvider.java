ab693e6cd94f393931e1ac065f4aa9cab14355e1
Ahmet Kutluğ Alperen Kars <akars20@ku.edu.tr>
2024-12-19 23:40:38 +0300

    pause functionality double check

--- src/main/java/com/rokue/game/input/GUIInputProvider.java
+++ src/main/java/com/rokue/game/input/GUIInputProvider.java
@@ -10,9 +10,15 @@ import java.util.List;
 
 public class GUIInputProvider extends KeyAdapter implements IInputProvider {
     private List<IAction> actions = new ArrayList<>();
+    private boolean isPaused = false;
+
 
     @Override
     public void keyPressed(KeyEvent e) {
+
+        if (isPaused) {
+            return; // Ignore input while paused
+        }
         switch (e.getKeyCode()) {
             case KeyEvent.VK_UP:
                 actions.add(new MoveAction(MoveAction.Direction.UP));
@@ -36,5 +42,14 @@ public class GUIInputProvider extends KeyAdapter implements IInputProvider {
         List<IAction> currentActions = new ArrayList<>(actions);
         actions.clear();
         return currentActions;
+    }
+
+    public void pause() {
+        isPaused = true;
+        actions.clear(); // Clear pending actions
+    }
+
+    public void resume() {
+        isPaused = false;
     }
 }
